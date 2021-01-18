b341a8c35681f66d57b57405f44abe9e1c3f217b
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-18 20:43:04 +0000

    Not sure

--- .docsifytopdfrc.js
+++ .docsifytopdfrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   contents: [ "docs/_sidebar.md" ], // array of "table of contents" files path
   pathToPublic: "pdf/readme.pdf", // path where pdf will stored
-removeTemp: true, // remove generated .md and .html or not
-  emulateMedia: "print", // mediaType, emulating by puppeteer for rendering pdf, 'print' by default (reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageemulatemediamediatype)
+  removeTemp: true, // remove generated .md and .html or not
+  emulateMedia: "screen", // mediaType, emulating by puppeteer for rendering pdf, 'print' by default (reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageemulatemediamediatype)
+  pathToDocsifyEntryPoint: "./docs"
 }
