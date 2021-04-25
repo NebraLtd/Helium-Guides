fd0d40c787117bee88e05361557d3670285122c9
Ryan Walmsley <ryan@ryanteck.uk>
2021-04-25 19:12:01 +0100

    Change to pdf-gen to make it clearer.

---
+++ pdf-gen/gen-pdfs.sh
@@ -0,0 +1,4 @@
+pandoc pdf-gen/pdf-data.yml  ../docs/indoor-hotspot/overview.md -o ../docs/pdfs/indoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
+pandoc pdf-gen/pdf-data.yml  ../docs/indoor-hotspot/quick-start.md -o ../docs/pdfs/indoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
+pandoc pdf-gen/pdf-data.yml  ../docs/outdoor-hotspot/overview.md -o ../docs/pdfs/outdoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
+pandoc pdf-gen/pdf-data.yml  ../docs/outdoor-hotspot/quick-start.md -o ../docs/pdfs/outdoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
--- pdf/gen-pdfs.sh
+++
@@ -1,4 +0,0 @@
-pandoc pdf-data.yml  ../docs/indoor-hotspot/overview.md -o ../docs/pdfs/indoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
-pandoc pdf-data.yml  ../docs/indoor-hotspot/quick-start.md -o ../docs/pdfs/indoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
-pandoc pdf-data.yml  ../docs/outdoor-hotspot/overview.md -o ../docs/pdfs/outdoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
-pandoc pdf-data.yml  ../docs/outdoor-hotspot/quick-start.md -o ../docs/pdfs/outdoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
