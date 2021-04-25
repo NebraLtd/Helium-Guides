14a2ea123feaed3329f5cdfd1d450778e350a2d5
Ryan Walmsley <ryan@ryanteck.uk>
2021-04-25 19:02:01 +0100

    Move fonts to tidy, add output directory.

--- pdf/gen-pdfs.sh
+++ pdf/gen-pdfs.sh
@@ -1,4 +1,4 @@
-pandoc pdf-data.yml  ../docs/indoor-hotspot/overview.md -o indoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
-pandoc pdf-data.yml  ../docs/indoor-hotspot/quick-start.md -o indoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
-pandoc pdf-data.yml  ../docs/outdoor-hotspot/overview.md -o outdoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
-pandoc pdf-data.yml  ../docs/outdoor-hotspot/quick-start.md -o outdoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
+pandoc pdf-data.yml  ../docs/indoor-hotspot/overview.md -o pdf-output/indoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
+pandoc pdf-data.yml  ../docs/indoor-hotspot/quick-start.md -o pdf-output/indoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
+pandoc pdf-data.yml  ../docs/outdoor-hotspot/overview.md -o pdf-output/outdoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
+pandoc pdf-data.yml  ../docs/outdoor-hotspot/quick-start.md -o pdf-output/outdoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
