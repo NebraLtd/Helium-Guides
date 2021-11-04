d358307f8be7cf944ff5135ce74f0e31cdcca2d2
Aaron Shaw <shawaj@gmail.com>
2021-11-04 21:36:00 +0000

    Update gen-pdfs.sh

--- pdf-gen/gen-pdfs.sh
+++ pdf-gen/gen-pdfs.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 pandoc pdf-data.yml  ../docs/indoor-hotspot/overview.md -o ../docs/pdfs/indoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
 pandoc pdf-data.yml  ../docs/indoor-hotspot/quick-start.md -o ../docs/pdfs/indoor-quick-start.pdf --pdf-engine=xelatex --resource-path ../docs/media/
 pandoc pdf-data.yml  ../docs/outdoor-hotspot/overview.md -o ../docs/pdfs/outdoor-overview.pdf --pdf-engine=xelatex --resource-path ../docs/media/
