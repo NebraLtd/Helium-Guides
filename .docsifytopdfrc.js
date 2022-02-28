02b07e35d0f495b3c8ac6a2e68bfc99ef1c700b7
Aaron Shaw <shawaj@gmail.com>
2022-02-28 01:28:54 +0000

    no longer needed

--- .docsifytopdfrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  contents: [ "docs/_sidebar.md" ], // array of "table of contents" files path
-  pathToPublic: "pdf/readme.pdf", // path where pdf will stored
-  removeTemp: true, // remove generated .md and .html or not
-  emulateMedia: "screen", // mediaType, emulating by puppeteer for rendering pdf, 'print' by default (reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageemulatemediamediatype)
-  pathToDocsifyEntryPoint: "./docs"
-}
